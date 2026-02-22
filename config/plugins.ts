c9244bdd6ec6c0c4f5c05cbb30de06af001f0d3e
CoderRCAman <www.amank.o.12345@gmail.com>
2026-02-22 12:06:04 +0530

    added node mailer support

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,24 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
-
+const config = 
+({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env.int('SMTP_PORT', 587),
+        secure: false, // Use `true` for port 465
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: 'no-reply@example.com',
+        defaultReplyTo: 'support@example.com',
+      },
+    },
+  },
+});
 export default config;
